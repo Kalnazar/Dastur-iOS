da53983518cbe88bd19315fc5a3b97480b4e1ce9
Sayat Kalnazar <kalnazar.sayat@gmail.com>
2023-05-22 15:38:46 +0600

    Initial Commit

---
+++ Dastur/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Dastur
+//
+//  Created by Саят Калназар on 22.05.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
