f19cf542e69b66c8cc43b4bc211c100830e8713f
Sayat Kalnazar <kalnazar.sayat@gmail.com>
2023-05-23 20:55:43 +0600

    UI of Authorization section was done

---
+++ Dastur/Controllers/Auth/SignInViewController.swift
@@ -0,0 +1,16 @@
+//
+//  SignInViewController.swift
+//  Dastur
+//
+//  Created by Саят Калназар on 23.05.2023.
+//
+
+import UIKit
+
+class SignInViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
---
+++ Dastur/Controllers/Auth/SignUpViewController.swift
@@ -0,0 +1,16 @@
+//
+//  SignUpViewController.swift
+//  Dastur
+//
+//  Created by Саят Калназар on 23.05.2023.
+//
+
+import UIKit
+
+class SignUpViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
